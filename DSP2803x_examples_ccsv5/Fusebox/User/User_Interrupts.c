ccf8c35c4cfb6c4664e354fdab98fbe6bf11a637
Bartho Horn <hornbartho@gmail.com>
2020-11-02 11:27:17 +0200

    Added ADC filters and assigns the status to measurements

--- DSP2803x_examples_ccsv5/Fusebox/User/User_Interrupts.c
+++ DSP2803x_examples_ccsv5/Fusebox/User/User_Interrupts.c
@@ -27,6 +27,7 @@ __interrupt void cpu_timer0_isr(void)
 __interrupt void cpu_timer1_isr(void)                           //50Hz loop
 {
     //maybe run in a function that is triggered by this counter?
+    //also, make more efficent by using Uint16 instead of floats
 
     static float Temp_Values[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     float Fuse_1_Signal;
@@ -57,19 +58,19 @@ __interrupt void cpu_timer1_isr(void)                           //50Hz loop
     //channel B2 - SOC1
     Fuse_1_Signal = Temp_Values[1] + (0.72*(((AdcResult.ADCRESULT1))-Temp_Values[1]));                          //add in proper gain
     Temp_Values[1] = Fuse_1_Signal;
-    if(Fuse_1_Signal>2048){
-        //fuse signal 1 = 1;
-    }
+    if(Fuse_1_Signal>2048)
+        Fuse_Out_Sense_1 = 0;
     else
-    {
-         //fuse signal 1 = 0;
-    }
+        Fuse_Out_Sense_1 = 1;                       //active when voltage is low
 
     //Fuse_Out_4
     //channel B4  - SOC2
     Fuse_4_Signal = Temp_Values[2] + (0.72*(((AdcResult.ADCRESULT2))-Temp_Values[2]));                          //add in proper gain
     Temp_Values[2] = Fuse_4_Signal;
-    //add if to give binary output
+    if(Fuse_4_Signal>2048)
+        Fuse_Out_Sense_4 = 0;
+    else
+        Fuse_Out_Sense_4 = 1;                       //active when voltage is low
 
     //Fuse box Temperature
     //channel A1 - SOC3
@@ -82,62 +83,108 @@ __interrupt void cpu_timer1_isr(void)                           //50Hz loop
     //channel A2 - SOC4
     Forward_Signal = Temp_Values[4] + (0.72*(((AdcResult.ADCRESULT4))-Temp_Values[4]));                           //add in proper gain
     Temp_Values[4] = Forward_Signal;
+    if(Forward_Signal>2048)
+        Forward_In_Sense = 1;                           //active high
+    else
+        Forward_In_Sense = 0;
 
     //Brake light output
     //channel A3 - SOC5
     Brake_Signal = Temp_Values[5] + (0.72*(((AdcResult.ADCRESULT5))-Temp_Values[5]));                           //add in proper gain
     Temp_Values[5] = Brake_Signal;
+    if(Brake_Signal>2048)
+        Brake_In_Sense = 1;                           //active high
+    else
+        Brake_In_Sense = 0;
 
     //Key switch input
     //channel A4 - SOC6
     Key_Sense_Signal = Temp_Values[6] + (0.72*(((AdcResult.ADCRESULT6))-Temp_Values[6]));                           //add in proper gain
     Temp_Values[6] = Key_Sense_Signal;
+    if(Key_Sense_Signal>2048)
+        Key_In_Sense = 1;                           //active high
+    else
+        Key_In_Sense = 0;
 
     //Horn output
     //channel A5 - SOC7
     Horn_Signal = Temp_Values[7] + (0.72*(((AdcResult.ADCRESULT7))-Temp_Values[7]));                           //add in proper gain
     Temp_Values[7] = Horn_Signal;
+    if(Horn_Signal>2048)
+        Horn_Ctrl_Sense = 1;                           //active high
+    else
+        Horn_Ctrl_Sense = 0;
 
     //E-stop
     //channel A6 - SOC8
     E_stop_Signal = Temp_Values[8] + (0.72*(((AdcResult.ADCRESULT8))-Temp_Values[8]));                           //add in proper gain
     Temp_Values[8] = E_stop_Signal;
+    if(E_stop_Signal>2048)
+        E_Stop_In_Sense = 1;                           //active high
+    else
+        E_Stop_In_Sense = 0;
 
     //Position Switch
     //channel A7 - SOC9
     Position_Signal = Temp_Values[9] + (0.72*(((AdcResult.ADCRESULT9))-Temp_Values[9]));                           //add in proper gain
     Temp_Values[9] = Position_Signal;
+    if(Position_Signal>2048)
+        Position_Out_Sense = 1;                           //active high
+    else
+        Position_Out_Sense = 0;
 
     //Relay 3                           -- needs to swop with relay 3 output (Heated seats)
     //channel B0 - SOC10
     Relay_3_Signal = Temp_Values[10] + (0.72*(((AdcResult.ADCRESULT10))-Temp_Values[10]));                           //add in proper gain
     Temp_Values[10] = Relay_3_Signal;
+    if(Relay_3_Signal>2048)
+        Heated_Seats_Out_Sense = 1;                           //active high
+    else
+        Heated_Seats_Out_Sense = 0;
 
     //Relay 4                            -- needs to swop to relay 4 output (Radio out)
     //channel B1 - SOC11
     Relay_4_Signal = Temp_Values[11] + (0.72*(((AdcResult.ADCRESULT11))-Temp_Values[11]));                           //add in proper gain
     Temp_Values[11] = Relay_4_Signal;
+    if(Relay_4_Signal>2048)
+        Radio_Out_Sense = 1;                           //active high
+    else
+        Radio_Out_Sense = 0;
 
     //12V Regulator - secondary
     //channel B3- SOC12
     V_Reg_Signal = Temp_Values[12] + (0.72*(((AdcResult.ADCRESULT12))-Temp_Values[12]));                           //add in proper gain
     Temp_Values[12] = V_Reg_Signal;
+    if(V_Reg_Signal>2048)
+        V_Reg_In_Sense = 1;                           //active high
+    else
+        V_Reg_In_Sense = 0;
 
     //Fuse_Out_7
     //channel B5 - SOC13
     Fuse_7_Signal = Temp_Values[13] + (0.72*(((AdcResult.ADCRESULT13))-Temp_Values[13]));                           //add in proper gain
     Temp_Values[13] = Fuse_7_Signal;
+    if(Fuse_7_Signal>2048)
+        Fuse_Out_Sense_7 = 0;                           //active low
+    else
+        Fuse_Out_Sense_7 = 1;
 
     //Fuse_Out_2
     //channel B6 - SOC14
     Fuse_2_Signal = Temp_Values[14] + (0.72*(((AdcResult.ADCRESULT14))-Temp_Values[14]));                           //add in proper gain
     Temp_Values[14] = Fuse_2_Signal;
-
+    if(Fuse_2_Signal>2048)
+        Fuse_Out_Sense_2 = 0;                           //active low
+    else
+        Fuse_Out_Sense_2 = 1;
     //Fuse_Out_8
     //channel B7 - SOC15
     Fuse_8_Signal = Temp_Values[15] + (0.72*(((AdcResult.ADCRESULT15))-Temp_Values[15]));                           //add in proper gain
     Temp_Values[15] = Fuse_8_Signal;
-
+    if(Fuse_8_Signal>2048)
+        Fuse_Out_Sense_8 = 0;                           //active low
+    else
+        Fuse_Out_Sense_8 = 1;
 
 	CpuTimer1.InterruptCount++;
 	EDIS;
